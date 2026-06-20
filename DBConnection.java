6fcb8960fbef278482c5eb6535b34d62a28d859e
Jatin <153608918+Jatin6280@users.noreply.github.com>
2026-06-20 23:50:17 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -1,17 +1,23 @@
+ package com.company;
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+import java.sql.*;
 public class DBConnection {
 
-  
+    private static final String URL = "jdbc:mysql://localhost:3306/bank_management";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Jatin6280492168";
 
-    public static void main(String[] args) {
+    public static Connection   getConnection() {
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("✅ Database Connected Successfully!");
-            conn.close();
+
+            return conn;
         } catch (Exception e) {
             System.out.println("❌ Connection Failed: " + e.getMessage());
+            return null;
         }
     }
 }

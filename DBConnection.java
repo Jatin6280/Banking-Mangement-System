7c394f17e9a1f82f20c6aeb908a3c941c8c70869
Jatin <153608918+Jatin6280@users.noreply.github.com>
2026-06-20 19:27:30 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -3,9 +3,7 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/bank_management";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Jatin6280492168";
+  
 
     public static void main(String[] args) {
         try {

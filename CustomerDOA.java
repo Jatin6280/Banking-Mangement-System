f723bee919b6d37c33f9be4fabf2be381d06be20
Jatin <153608918+Jatin6280@users.noreply.github.com>
2026-06-20 23:50:40 +0530

    Update CustomerDOA.java

--- CustomerDOA.java
+++ CustomerDOA.java
@@ -1,5 +1,33 @@
+package com.company;
+
+import com.company.Customer;
+
+
+
 import java.sql.*;
 
- class CustomerDAO {
+class CustomerDAO {
 
+    Connection conn;
+
+    public CustomerDAO() {
+        conn = DBConnection.getConnection();
+    }
+
+    public void addCustomer(Customer c){
+
+        String query = "INSERT INTO customer (customer_id, email, name) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps =conn.prepareStatement(query);
+            ps.setString(1,c.getCustomerId());
+            ps.setString(2, c.getEmail());
+            ps.setString(3, c.getName());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
